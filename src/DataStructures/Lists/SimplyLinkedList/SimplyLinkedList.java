2513f6ac8e805607c8460750018746e9b4d9a81d
AntonisZks <antoniszks2003@gmail.com>
2024-02-23 21:21:41 +0200

    Started the creation of the simply linked list data structure

--- src/DataStructures/Lists/SimplyLinkedList/SimplyLinkedList.java
+++ src/DataStructures/Lists/SimplyLinkedList/SimplyLinkedList.java
@@ -1,6 +1,83 @@
+/* Filename: SimplyLinkedList.java */
+
 package DataStructures.Lists.SimplyLinkedList;
 
-public class SimplyLinkedList 
+/**
+ * A class that represents a simply linked list data structure
+ * @author Antonis Zikas
+ */
+public class SimplyLinkedList<list_t>
 {
+    private static final String DEFAULT_SEPERATOR = " -> "; // Default seperator character for the data printing
+
+    // Data of the simply linked list
+    private ListNode<list_t> head;
+    private int size;
+
+    /**
+     * Constructor of the singly linked list
+     */
+    public SimplyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    /**
+     * Inserts data to the end of the simply linked list
+     * @param data the data to be inserted
+     */
+    public void insertDataAtEnd(list_t data)
+    {
+        // Creating a new simply linked list node
+        ListNode<list_t> newNode = new ListNode<list_t>(data, null);     
         
+        // Checking if the list is empty, If so add the new node and update the size of the list
+        if (this.head == null) {
+            this.head = newNode; this.size++;
+            return;
+        } 
+        // Initialize a current node and loop until the very end of the list
+        ListNode<list_t> currentNode = this.head;
+
+        while (currentNode.getNextNode() != null) {
+            currentNode = currentNode.getNextNode();
+        }
+        // Set the last node of the list and update its size
+        currentNode.setNextNode(newNode); this.size++;
+
+        return;
+    }
+
+    /**
+     * Advanced printing of the simply linked list's sequence of data
+     * @param seperator a character seperator for the data printing
+     */
+    public void print(String seperator)
+    {
+        System.out.print("[");
+
+        // Checking if the list is empty
+        if (this.head == null) { System.out.println("]"); return; }
+
+        ListNode<list_t> currentNode = this.head;
+
+        // Loop through every node in the list and print its content
+        while (currentNode != null) 
+        {
+            System.out.print(currentNode.getData());
+            if (currentNode.getNextNode() != null) { // Printing a seperator character if necessary
+                System.out.print(seperator); 
+            }
+
+            currentNode = currentNode.getNextNode();
+        }
+        System.out.println("]");
+    }
+
+    /**
+     * Default printing of the simply linked list's sequence of data
+     */
+    public void print() {
+        this.print(SimplyLinkedList.DEFAULT_SEPERATOR);
+    }
 }
--- src/Test1.java
+++ src/Test1.java
@@ -1,15 +1,15 @@
-import DataStructures.Lists.SimplyLinkedList.ListNode;
 import DataStructures.Lists.SimplyLinkedList.SimplyLinkedList;
 
-
 public class Test1 {
-    public static void main(String[] args) {
-        ListNode<Integer> myNode = new ListNode<Integer>(10, null);
-        ListNode<Integer> myNode2 = new ListNode<Integer>(15, null);
+    public static void main(String[] args)
+    {
+        SimplyLinkedList<Integer> myList = new SimplyLinkedList<Integer>();
 
-        myNode.setNextNode(myNode2);
+        for (int i = 0; i < 10; i++) {
+            myList.insertDataAtEnd(i + 1);
+        }
 
-        myNode.print();
-        myNode.getNextNode().print();
+        myList.print();
+        myList.print(", ");
     }
 }
